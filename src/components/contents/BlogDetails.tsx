aa47a6f0d98f92fa512c2182a2e2003a161f0309
Solomon Njobvu <solomonnjobvu1@gmail.com>
2024-01-23 21:26:57 +0200

    CRUD functionality done

--- src/components/contents/BlogDetails.tsx
+++ src/components/contents/BlogDetails.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { BlogsContext } from "../../store/blogs-context";
 import { FiArrowLeft } from "react-icons/fi";
@@ -6,10 +6,27 @@ import { FiArrowLeft } from "react-icons/fi";
 export default function BlogDetails(): JSX.Element {
   const params = useParams();
 
-  const blogsCxt = useContext(BlogsContext);
-  const { blogs } = blogsCxt;
+  const blogCxt = useContext(BlogsContext);
+  const { blogs, removeABlog, updateABlog, isEditing, setIsEditing } = blogCxt;
 
   const blog = blogs.find((blog) => blog.id === params.id);
+  const [updateBlog, setUpdateBlog] = useState({
+    title: blog?.title,
+    content: blog?.content,
+  });
+
+  function updatingHandler(identifier, value) {
+    setUpdateBlog({
+      ...updateBlog,
+      [identifier]: value,
+    });
+  }
+
+  const handleSubmitBlog = (event: FormEvent): void => {
+    event.preventDefault();
+    updateABlog(params.id, updateBlog);
+    setIsEditing(false);
+  };
 
   return (
     <>
@@ -18,11 +35,56 @@ export default function BlogDetails(): JSX.Element {
           <FiArrowLeft />
           Blogs
         </Link>
-        <h1 className="text-2xl font-bold py-5">{blog.title}</h1>
-        <p className="text-stone-500 pb-1 border-b">{blog.date}</p>
-        <article className="whitespace-pre-line break-words py-10">
-          {blog.content}
-        </article>
+        {!isEditing && (
+          <>
+            <h1 className="text-2xl font-bold py-5">{blog.title}</h1>
+            <p className="text-stone-500 pb-1 border-b">{blog.date}</p>
+            <article className="whitespace-pre-line break-words py-10">
+              {blog.content}
+            </article>
+          </>
+        )}
+        {isEditing && (
+          <form onSubmit={handleSubmitBlog} className="w-[80%] mx-4">
+            <div>
+              <label className="block" htmlFor="title">
+                Title
+              </label>
+              <input
+                className="border border-stone-300 w-full"
+                type="text"
+                placeholder="Title"
+                required
+                value={updateBlog.title}
+                onChange={(event) =>
+                  updatingHandler("title", event.target.value)
+                }
+              />
+            </div>
+            <div>
+              <label className="block" htmlFor="title">
+                Content
+              </label>
+              <textarea
+                className="border border-stone-300 w-full h-[60vh]"
+                rows={5}
+                placeholder="Title"
+                required
+                value={updateBlog.content}
+                onChange={(event) =>
+                  updatingHandler("content", event.target.value)
+                }
+              ></textarea>
+            </div>
+            <button>Save</button>
+          </form>
+        )}
+        {!isEditing && (
+          <>
+            <button onClick={() => removeABlog(params.id)}>Delete</button>
+            <button onClick={() => setIsEditing(!isEditing)}>Edit blog</button>
+          </>
+        )}
       </section>
     </>
   );
--- src/store/blogs-context.tsx
+++ src/store/blogs-context.tsx
@@ -13,11 +13,17 @@ interface BlogIF {
 interface ContextValueIF {
   blogs: BlogIF[];
   addNewBlog: (data: BlogIF) => void;
+  removeABlog: (id: string) => void;
+  updateABlog: (id: string) => void;
+  isEditing: boolean;
 }
 
 export const BlogsContext = createContext<ContextValueIF>({
   blogs: [],
   addNewBlog: () => {},
+  removeABlog: () => {},
+  updateABlog: () => {},
+  isEditing: false,
 });
 
 export default function BlogsContextProvider({
@@ -25,24 +31,50 @@ export default function BlogsContextProvider({
 }: BlogsContextProviderIF): JSX.Element {
   const [blogs, setBlogs] = useState(() => {
     const blogStored = localStorage.getItem("blogs");
-
     return blogStored ? JSON.parse(blogStored) : [];
   });
+
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     localStorage.setItem("blogs", JSON.stringify(blogs));
   }, [blogs]);
 
   function addNewBlog(data: BlogIF) {
-    console.log(data);
     setBlogs((prevBlogs: BlogIF[]) => {
       return [data, ...prevBlogs];
     });
   }
 
+  function removeABlog(id: string): void {
+    const filterdBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(filterdBlogs);
+  }
+
+  function updateABlog(identifier: string, data: BlogIF) {
+    let blog = blogs.find((item) => item.id === identifier);
+
+    if (blog) {
+      blog = {
+        ...blog,
+        title: data.title,
+        content: data.content,
+        // date: data.date, // Include the date in the update
+      };
+
+      setBlogs((prevState) => {
+        return [blog, ...prevState.filter((item) => item.id !== identifier)];
+      });
+    }
+  }
+
   const valueCxt: ContextValueIF = {
     blogs,
     addNewBlog,
+    removeABlog,
+    updateABlog,
+    isEditing,
+    setIsEditing,
   };
   return (
     <BlogsContext.Provider value={valueCxt}>{children}</BlogsContext.Provider>
